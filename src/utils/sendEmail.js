f5166ee57cb12c38f9e5490194d913b7910bbca7
ByashPaswan <byas.paswan99@gmail.com>
2025-10-02 14:41:59 +0530

    integrate sendGrid for email

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -1,15 +1,22 @@
 const nodemailer = require('nodemailer');
 async function sendVerificationEmail(to, subject, text) {
     const transporter = nodemailer.createTransport({
-        service: 'gmail', 
+        // service: 'gmail', 
+        // auth: {
+        //     user: process.env.GMAIL_USER_EMAIL,
+        //     pass: process.env.GMAIL_APP_PASSWORD
+        // }
+        host: "smtp.sendgrid.net",
+        port: 587,
+        secure: false, // true for 465, false for 587
         auth: {
-            user: process.env.GMAIL_USER_EMAIL,
-            pass: process.env.GMAIL_APP_PASSWORD
+            user: 'apikey', // literally "apikey"
+            pass: process.env.SENDGRID_API_KEY
         }
     });
 
     const mailOptions = {
-        from: process.env.GMAIL_USER_EMAIL,
+        from:  '"capitalMart" <no-reply@capitalmart.netlify.app>' ,  ///process.env.GMAIL_USER_EMAIL,
         to,
         subject,
         text
